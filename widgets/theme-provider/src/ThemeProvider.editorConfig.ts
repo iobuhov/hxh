4b9c201e2cd880ad1cf296e0a37d9ad38f006317
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-08-13 15:48:47 +0200

    feat: add theme provider

---
+++ widgets/theme-provider/src/ThemeProvider.editorConfig.ts
@@ -0,0 +1,6 @@
+import { ThemeProviderPreviewProps } from "../typings/ThemeProviderProps";
+import { Properties } from "../typings/editor-types";
+
+export function getProperties(_values: ThemeProviderPreviewProps, schema: Properties): Properties {
+    return schema;
+}
